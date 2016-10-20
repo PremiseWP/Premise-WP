269c5473d4122f1a1758b22b09b3632a6e6a7ea9
Mario Vallejo <mario@vallgroup.com>
2016-10-20 08:55:36 -0500

    new premise field duplicate

--- js/source/premise-field-duplicate.js
+++ js/source/premise-field-duplicate.js
@@ -21,6 +21,7 @@
 			this.each(function() {
 				$(this).premiseFieldDuplicate( options );
 			});
+			return;
 		}
 
 		// Reference variables.
@@ -33,15 +34,63 @@
 
 		// Make sure we have everything we need to work with.
 		var _construct = function() {
+			// build our default element
+			buildDuplicator();
+			// get required and non required fields
+			findFields();
+			// add index to each fields container and add remove button if needed
+			resetCountIndex();
+			// make sure fields are being serialized
+			// serializeFieldsIntoArray();
+			// bind our actions based on the options passed
+			( opts.autoDuplicate )    ? requiredFields.change( tryClone )            : false;
+			( opts.displayAddButton ) ? $el.find( '.pwpfd-clone' ).click( tryClone ) : false;
+		},
 
-			// prepare our element
-			if ( ! $el.parents( '.pwpfd-wrapper' ).length ) $el.wrap( '<div class="pwpfd-wrapper"></div>' );
-			$el.children().wrapAll( '<div class="pwpfd-duplicate-this" data-duplicate-count="'+opts.startCount+'"></div>' );
+		// try to clone
+		tryClone = function( e ) {
+			e.preventDefault();
+			if ( ! reqFieldsEmpty() ) {
+				_clone();
+			}
+			return false;
+		},
+
+		// clone the fields
+		_clone = function() {
+			var subject = $el.find( '.pwpfd-duplicate-this' ),
+			_count      = subject.attr( 'data-duplicate-count' ),
+			clone       = subject.clone(),
+			count       = ( +_count + 1 );
+
+			subject.attr( 'data-duplicate-count', count );
+
+			fields.each( function() {
+				$(this).val('');
+			} );
+
+			serializeFieldsIntoArray( fields, _count, count );
+
+			$el.before( clone.addClass( 'pwpfd-duplicated' ).append( removeBtn ) );
+
+			maybeBindRemove();
+		};
+
+		// run our code
+		_construct();
+
+		return this;
+
+		// build our main element to clone from
+		function buildDuplicator() {
 			$el.addClass( 'pwpfd-duplicator' );
+			$el.children().wrapAll( '<div class="pwpfd-duplicate-this"></div>' );
+			if ( ! $el.parents( '.pwpfd-wrapper' ).length ) $el.wrap( '<div class="pwpfd-wrapper"></div>' );
+			if ( opts.displayAddButton && ! $el.find( '.pwpfd-clone' ).length ) $el.append( cloneBtn );
+		}
 
-			// diplay the add button if applicable and does not exist
-			if ( opts.displayAddButton && ! $el.children( '.pwpfd-clone' ).length ) $el.append( cloneBtn );
-
+		// get all fields and required fields as well
+		function findFields() {
 			fields = $el.find( 'select, input, textarea' );
 
 			if ( fields.prop( 'required' ) ) {
@@ -54,63 +103,33 @@
 			}
 
 			requiredFields = $el.find( '.pwpfd-field' );
+		}
 
-			serializeFieldsIntoArray();
+		// resets the data duplicate count attribute in our duplicate containers
+		function resetCountIndex() {
+			var _index =  opts.startCount;
+			$el.parents( '.pwpfd-wrapper' ).find( '.pwpfd-duplicate-this' ).each( function() {
+				var $this = $( this ),
+				oldIndex  = $this.attr( 'data-duplicate-count' ) ? $this.attr( 'data-duplicate-count' ) : _index,
+				fields    = $this.find( 'select, input, textarea' );
 
-			if ( opts.autoDuplicate ) requiredFields.change( tryClone );
+				serializeFieldsIntoArray( fields, oldIndex, _index );
 
-			if ( opts.displayAddButton ) $el.find( '.pwpfd-clone' ).click( tryClone );
-		},
+				if ( ! $this.find( '.pwpfd-remove' ).length && ! $this.parents( '.pwpfd-duplicator' ).length ) $this.append( removeBtn );
 
-
-		_clone = function() {
-			var subject = $el.find( '.pwpfd-duplicate-this' ),
-			_count      = subject.attr( 'data-duplicate-count' ),
-			clone       = subject.clone();
-
-			var count = ( +_count + 1 );
-
-			subject.attr( 'data-duplicate-count', count );
-
-			fields.each( function() {
-				$(this).val('');
+				$this.attr( 'data-duplicate-count', _index );
+				_index++;
 			} );
-
-			serializeFieldsIntoArray( _count, count );
-
-			$el.before( clone.addClass( 'pwpfd-duplicated' ).append( removeBtn ) );
-
-			$( '.pwpfd-remove' ).click( function( e ) {
-				e.preventDefault();
-				$(this).parent( '.pwpfd-duplicated' ).hide( 'fast', function() {
-					$(this).remove();
-				} );
-
-				// TODO reset all fields arrays
-
-				return false;
-			} );
-		},
-
-
-		tryClone = function( e ) {
-			e.preventDefault();
-			if ( ! reqFieldsEmpty() ) {
-				_clone();
-			}
-			return false;
-		};
-
-		/**
-		 * Pivate Helpers
-		 */
+			maybeBindRemove();
+		}
 
 		// make sure that fields and labels are serialized properly
-		function serializeFieldsIntoArray( index, replace ) {
-			index   = index || opts.startCount;
+		function serializeFieldsIntoArray( fields, index, replace ) {
+			fields = fields || [];
+			// index  = index  || $el.find( '.pwpfd-duplicate-this' ).attr( 'data-duplicate-count' );
 
-			var _match = new RegExp( "(\["+index+"\])", "g" ),
-			_idmacth   = new RegExp( "(-"+index+")$", "g" );
+			var _match = new RegExp( "(["+index+"])", "gi" ),
+			_idmacth   = new RegExp( "(-"+index+")$", "gi" );
 
 			fields.each( function( i, v ) {
 
@@ -121,9 +140,11 @@
 				if ( _name && '' !== _name ) {
 					// if the name is already serialized in an array
 					if ( _name.match( /^.*(\[|\]).*$/g ) ) {
-						if ( replace ) {
+						// if we have a replace index try replacing
+						if ( 'undefined' !== typeof replace ) {
 							$this.attr( 'name', _name.replace( _match, replace ) );
 						}
+						// no replace index, parse to the end
 						else if ( ! _name.match( _match ) ) {
 							$this.attr( 'name', _name + '['+index+']' );
 						}
@@ -136,34 +157,26 @@
 
 				if ( _id && '' !== _id ) {
 					if ( _id.match( _idmacth ) ) {
+						$el.parents( '.pwpfd-wrapper' ).find( 'label[for="'+_id+'"]' ).attr( 'for', _id.replace( _idmacth, replace ) );
 						$this.attr( 'id', _id.replace( _idmacth, replace ) );
 					}
 					else {
+						$el.parents( '.pwpfd-wrapper' ).find( 'label[for="'+_id+'"]' ).prop( 'for', _id + '-' + index );
 						$this.attr( 'id', _id + '-' + index );
 					}
 				}
 
 			} );
-
-			$el.find( 'label' ).each( function() {
-				var $this = $(this),
-				_for      = $this.attr( 'for' );
-				if ( _for && '' !== _for ) {
-					if ( _for.match( _idmacth ) ) {
-						$this.attr( 'for', _for.replace( _idmacth, replace ) );
-					}
-					else {
-						$this.attr( 'for', _for + '-' + index );
-					}
-				}
-			} );
 		}
 
-
+		// check if any of the required fields is empty
 		function reqFieldsEmpty() {
+			if ( opts.allowEmptyFields ) return false;
 			var a = false;
 			requiredFields.each( function() {
-				if ( ! $(this).val() || '' == $(this).val() ) {
+				var $this = $(this);
+				if ( ! $this.val() || '' == $this.val() ) {
+					$this.focus();
 					a = true;
 					return false;
 				}
@@ -171,18 +184,30 @@
 			return a;
 		}
 
-		_construct();
-
-		return this;
+		// bind remove button if any exist
+		function maybeBindRemove() {
+			var btn = $el.parents( '.pwpfd-wrapper' ).find( '.pwpfd-remove' );
+			if ( btn.length ) {
+				btn.off().click( function( e ) {
+					e.preventDefault();
+					$(this).parent( '.pwpfd-duplicate-this' ).hide( 'fast', function() {
+						$(this).remove();
+						resetCountIndex();
+					} );
+					return false;
+				} );
+			}
+		}
 	};
 
 
 	// Defaults: onCopy & onRemove callback functions.
 	$.fn.premiseFieldDuplicate.defaults = {
 		// options
-		displayAddButton: true, // displays and bind the add button
-		autoDuplicate:    true, // if true, when all required fields are filled out it will automatically duplicate
-		startCount:       1,    // begin the count at a different number than 0
+		displayAddButton: true,  // displays and bind the add button
+		autoDuplicate:    true,  // if true, when all required fields are filled out it will automatically duplicate
+		allowEmptyFields: false, // allow to clone with empty fields whther required or not
+		startCount:       0,     // begin the count at a different number than 0
 
 		// callbacks
 		onCopy :   function() { return true }, // currently does not do anything
